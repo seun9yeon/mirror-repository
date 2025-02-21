62bf6c345c599028b6ed2ecaaa4ea153122c4a87
apricity2u <gkdms9849@gmail.com>
2025-02-21 21:13:34 +0900

    [BK-5] [chore] spring boot 프로젝트 빌드 (#5)

---
+++ backend/src/main/java/org/example/book_report/BookReportApplication.java
@@ -0,0 +1,13 @@
+package org.example.book_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReportApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/example/book_report/BookReportApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.book_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
