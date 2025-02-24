09a5dc0376415411d731cdd5d034d841982aa718
CloakingGhost <project0858@gmail.com>
2025-02-24 18:15:53 +0900

    Feature/bk 25 be 회원가입 (#17)
    * [BK-25] [chore] 회원가입 클래스 이름 변경
    
    * [BK-25] [chore] 타입 변경
    
    ---------
    
    Co-authored-by: ysc13245 <ysc13245@gmail.com>

--- backend/src/main/java/org/example/book_report/controller/AuthController.java
+++ backend/src/main/java/org/example/book_report/controller/AuthController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
-
     private final AuthService authService;
 
     @GetMapping("/test")
