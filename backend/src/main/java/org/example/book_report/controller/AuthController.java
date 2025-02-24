53ee52183fe70cb959a8f9a97d62f7f53d86291b
CloakingGhost <project0858@gmail.com>
2025-02-24 18:00:30 +0900

    Feature/bk 19 be security config (#14)
    * [BK-19] [chore] 시큐리티 설정
    기본 폴더 구조 형성
    - 유저를 기반으로 형성함
    
    * [BK-19] [chore] 시큐리티 설정중
    회원가입 기준으로 설정 중
    
    * [BK-19] [chore] 시큐리티 기본 설정 완료
    폴더 구조와, 시큐리티 설정
    - api/** 으로 모든 경로 열어 두었음
    - 비밀번호 인코딩 클래스 빈으로 등록
    - 생성일, 수정일 공통(common) 엔티티에 있음
    - 권한 1개만 존재 (ROLE_USER)
    
    Co-authored-by: CloakingGhost <project0858@gmail.com>
    
    * [BK-19] [chore] 글로벌 예외처리, API 응답 객체

--- backend/src/main/java/org/example/book_report/controller/AuthController.java
+++ backend/src/main/java/org/example/book_report/controller/AuthController.java
@@ -1,12 +1,10 @@
 package org.example.book_report.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.example.book_report.common.ApiResponse;
+
 import org.example.book_report.service.AuthService;
-import org.example.book_report.service.UserService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
-    private final UserService userService;
+
+    private final AuthService authService;
+
 
     @GetMapping("/test")
     public String test() {
         return "test";
     }
+
+
 }
