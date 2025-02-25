7c090f35b2f14037301a75e45d4987f6f3df2bed
cocobabb <cocobab05108@gmail.com>
2025-02-25 19:27:16 +0900

    [BK-49] [feat] store 설정 정리 (#36)
    JWT를 Cookie로 바꿈, Slice으로 JWT를 다루던 코드 삭제

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 
-import { Provider } from 'react-redux';
-import store from './store/store';
-
 export default function App() {
   return (
     <>
-      <Provider store={store}>
-        <RouterProvider router={router}></RouterProvider>
-      </Provider>
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 }
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import store from '../store/store';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
@@ -9,13 +8,5 @@ const instance = axios.create({
     'Content-Type': 'application/json',
   },
 });
-
-// instance.interceptors.request.use((config) => {
-//   const token = store.getState().auth.token;
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
 
 export default instance;
