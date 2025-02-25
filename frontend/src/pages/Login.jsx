b90ca412ad7f241c362e83b1d2df47b0a4ab511d
cocobabb <cocobab05108@gmail.com>
2025-02-25 14:45:44 +0900

    [BK-17] [feat] 기능만 구현한 로그인 화면 (#26)
    * [BK-17] [feat] 로그인 기본 구조 잡기
    
    * [BK-17] [feat] Axios를 이용한 API 통신 기능 추가
    
    * [BK-17] [feat] 로고 클릭 시 메인 화면으로 이동 기능
    
    * [BK-17] [feat] 회원가입 클릭 시 회원가입 화면으로 이동 기능
    
    * [BK-17] [feat] 공백 입력 방지 기능 구현
    
    * [BK-17] [feat] 유효성 검사
    
    * [BK-17] [refactor] 불필요한 Axios 요청 최소화
    
    * [BK-17] [refactor] 아이디 또는 비밀번호 오류 시 안내 메시지

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,92 @@
-import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const [usernameBlankCheck, setUsernameBlankCheck] = useState(false);
+  const [passwordBlankCheck, setPasswordBlankCheck] = useState(false);
+
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleFormInput = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value.trim(),
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (formData.username == '') {
+      setUsernameBlankCheck(true);
+    } else if (formData.username != '') {
+      setUsernameBlankCheck(false);
+    }
+
+    if (formData.password == '') {
+      setPasswordBlankCheck(true);
+    } else if (formData.password != '') {
+      setPasswordBlankCheck(false);
+    }
+
+    if (formData.username != '' && formData.password != '') {
+      try {
+        const response = await authApi.login(formData);
+        const data = response.data;
+
+        const { token, status, error } = data.data;
+        navigate('/');
+      } catch {
+        console.error();
+      }
+    }
+  };
+
+  return (
+    <>
+      <div>Login</div>
+      <label htmlFor="logo">
+        <h1>
+          <input type="button" id="logo" onClick={() => navigate(`/`)} hidden />
+          로고
+        </h1>
+      </label>
+      <form action="" name="formData" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          placeholder="아이디"
+          onChange={handleFormInput}
+        />
+        {usernameBlankCheck && <div>필수 입력 값입니다</div>}
+
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          placeholder="비밀번호"
+          onChange={handleFormInput}
+        />
+        {passwordBlankCheck && <div>필수 입력 값입니다</div>}
+
+        <br />
+        {error && <div>아이디와 비밀번호를 확인해주세요</div>}
+        <button>로그인</button>
+      </form>
+
+      <div>
+        아직 계정이 없으신가요?
+        <label htmlFor="signup">
+          <input type="button" id="signup" onClick={() => navigate(`/signup`)} hidden />
+          회원가입
+        </label>
+      </div>
+    </>
+  );
 }
