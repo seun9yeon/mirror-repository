81ee6fabd039850bfd7f94e8cb6eae002f2aee71
apricity2u <gkdms9849@gmail.com>
2025-02-25 15:17:56 +0900

    [BK-23] [feat] 회원가입 Api 요청 & 아이디 중복 확인 및 비밀번호 일치여부 확인 기능 구현 (#28)
    * [BK-23] [refactor] 회원가입 폼 입력값 받기
    
    * [BK-23] [feat] 회원가입 폼 제출
    
    * [BK-23] [feat] 전화번호 입력값 추가하여 회원가입 api 요청
    
    * [BK-23] [feat] 아이디 중복확인 및 비밀번호 일치여부 확인 기능 구현

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,52 +1,163 @@
 import React, { useState } from 'react';
 import authApi from '../api/authApi';
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
-  const [username, setUsername] = useState('');
+  const navigate = useNavigate();
+
+  // 전화번호 기록용 state
+  const [phoneNumberInput, setPhoneNumberInput] = useState({
+    phone1: '',
+    phone2: '',
+    phone3: '',
+  });
+
+  // 회원가입 폼 데이터 제출용 state
+  const [userData, setUserData] = useState({
+    username: '',
+    password: '',
+    name: '',
+    phoneNumber: '',
+  });
+
   // 아이디 중복확인용 state
   const [isUsed, setIsUsed] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
 
-  // 아이디 중복확인용 api
-  async function handleDuplicatedUsername() {
+  // 비밀번호 확인용 state
+  const [isEqual, setIsEqual] = useState(false);
+
+  // 아이디 중복확인용 함수
+  async function handleUsedUsername() {
     try {
       const response = await authApi.verifyUsername(username);
       const data = response.data;
       setIsUsed(data.used);
-    } catch (e) {
-      e.message('입력값을 확인해주세요');
+      if (!isUsed) {
+        setIsChecked(true);
+      }
+    } catch {
+      console.error('입력값을 확인해주세요');
     }
   }
 
-  function handleInputUsername(e) {
-    setUsername(e.target.value);
+  // 비밀번호 확인용 함수
+  function handlePasswordCheck(e) {
+    if (userData.password == e.target.value) {
+      setIsEqual(true);
+    } else {
+      setIsEqual(false);
+    }
   }
+
+  // 회원가입 폼 제출용 함수
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    // 아이디 중복확인이 완료되지 않으면 폼 제출 불가
+    if (isUsed && !isChecked) {
+      return alert('아이디 중복확인 버튼을 눌러주세요.');
+    }
+
+    // 비밀번호가 일치하지 않으면 폼 제출 불가
+    if (!isEqual) {
+      return alert('비밀번호가 일치하지 않습니다.');
+    }
+
+    try {
+      await authApi.signup(userData);
+      navigate('/login');
+    } catch {
+      console.error('입력값을 확인해주세요');
+    }
+  }
+
+  // 회원가입 폼 입력값
+  function handleFormInput(e) {
+    const { name, value } = e.target;
+    setUserData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  // 전화번호 입력값
+  function handlePhoneNumberInput(e) {
+    const { name, value } = e.target;
+
+    const updatedPhoneNumberInput = {
+      ...phoneNumberInput,
+      [name]: value,
+    };
+
+    setPhoneNumberInput(updatedPhoneNumberInput);
+
+    const { phone1, phone2, phone3 } = updatedPhoneNumberInput;
+
+    setUserData((prev) => ({
+      ...prev,
+      phoneNumber: phone1 + phone2 + phone3,
+    }));
+  }
+
   return (
     <>
       <h1>로고</h1>
-      <form>
+      <form
+        onSubmit={handleSubmit}
+        style={{ display: 'flex', flexDirection: 'column', gap: '5px' }}
+      >
         <label htmlFor="username">아이디</label>
         <div>
           <input
             type="text"
             name="username"
             id="username"
-            value={username}
-            onChange={handleInputUsername}
+            value={userData.username}
+            onChange={handleFormInput}
           />
-          <button onClick={handleDuplicatedUsername}>중복확인</button>
+          <button onClick={handleUsedUsername}>중복확인</button>
           {isUsed && <div>이미 사용중인 아이디입니다.</div>}
         </div>
         <label htmlFor="password">비밀번호</label>
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={userData.password}
+          onChange={handleFormInput}
+        />
         <label htmlFor="checkPassword">비밀번호 확인</label>
-        <input type="password" name="checkPassword" id="checkPassword" />
+        <input
+          type="password"
+          name="checkPassword"
+          id="checkPassword"
+          onChange={handlePasswordCheck}
+        />
         <label htmlFor="name">이름</label>
-        <input type="text" name="name" id="name" />
+        <input type="text" name="name" id="name" value={userData.name} onChange={handleFormInput} />
         <label htmlFor="phone1">전화번호</label>
         <div>
-          <input type="tel" id="phone1" name="phoneNumber" /> -
-          <input type="tel" id="phone2" name="phoneNumber" /> -
-          <input type="tel" id="phone3" name="phoneNumber" />
+          <input
+            type="tel"
+            id="phone1"
+            name="phone1"
+            value={phoneNumberInput.phone1}
+            onChange={handlePhoneNumberInput}
+          />
+          -
+          <input
+            type="tel"
+            id="phone2"
+            name="phone2"
+            value={phoneNumberInput.phone2}
+            onChange={handlePhoneNumberInput}
+          />
+          -
+          <input
+            type="tel"
+            id="phone3"
+            name="phone3"
+            value={phoneNumberInput.phone3}
+            onChange={handlePhoneNumberInput}
+          />
         </div>
         <button>회원가입</button>
       </form>
