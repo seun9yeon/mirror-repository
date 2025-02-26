f3eb028fab9ae1006c623b58f0df7293686c61fd
cocobabb <cocobab05108@gmail.com>
2025-02-26 16:27:54 +0900

    [BK-50] [feat] 감상문 작성 화면 기본 구조 만들기 (#42)
    * [BK-50] [feat] 감상문 작성의 기본 구조, 도서 검색 axios연결
    
    * [BK-50] [feat] 감상문 작성 화면의 도서 검색 모달창 띄우기
    
    * [BK-50] [feat] 응답 데이터 구조 변경으로 코드 수정

--- frontend/src/pages/BookReview.jsx
+++ frontend/src/pages/BookReview.jsx
@@ -1,5 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import bookApi from '../api/bookApi';
 
 export default function BookReview() {
-  return <div>BookReview</div>;
+  const [searchBook, setSearchBook] = useState('');
+  const [bookImage, setBookImage] = useState('');
+  const [cardImage, setCardImage] = useState('');
+
+  // 도서 검색 관련
+  const [bookList, setBookList] = useState([]);
+
+  const handleSearchBookTitle = async (e) => {
+    setSearchBook(e.target.value);
+
+    try {
+      const response = await bookApi.searchBooks(searchBook);
+      const { hasNext, bookList } = response;
+
+      setBookList(bookList);
+    } catch {
+      console.error();
+    }
+  };
+  return (
+    <>
+      <input type="text" onChange={handleSearchBookTitle} />
+      {
+        <>
+          {bookList.length != 0 ? (
+            <ul>
+              {bookList.map((book) => (
+                <li key={book.bookId}>
+                  <img src={book.imgUrl} alt="" />
+                  <div>{book.title}</div>
+                  <div>{book.author}</div>
+                  <div>{book.publisher}</div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div>도서 검색 직접입력 모달창</div>
+          )}
+        </>
+      }
+
+      <img name="bookImage" src={bookImage} alt="" />
+      <input name="title" type="text" placeholder="도서 검색을 먼저 하세요" readOnly />
+      <input name="author" type="text" placeholder="도서 검색을 먼저 하세요" readOnly />
+      <input name="publisher" type="text" placeholder="도서 검색을 먼저 하세요" readOnly />
+
+      <img name="cardImage" src={cardImage} alt="" />
+      <input name="onelineTitle" type="text" />
+
+      <textarea name="" id=""></textarea>
+    </>
+  );
 }
