48b1c2159c95057aab4cc45830ffe3c3a2185df1
CloakingGhost <project0858@gmail.com>
2025-02-27 14:36:26 +0900

    Chore/bk 63 add 3 s environment (#48)
    * [BK-62] [feat] https 쿠키 생성 로직 변경
    
    - 쿠키 생성 시 scure 옵션을 사용하여 https 환경에서 동작하도록 함
    
    * [BK-63] [chore] S3 환경변수 추가
    
    - 리전 변수 추가

--- backend/src/main/java/org/example/book_report/dto/response/TokenResponseDto.java
+++ backend/src/main/java/org/example/book_report/dto/response/TokenResponseDto.java
@@ -17,9 +17,9 @@ public class TokenResponseDto {
         return ResponseCookie
                 .from(ACCESS_TOKEN, this.accessToken)
                 .httpOnly(true) // XSS(Cross site scripting) attack 방지, 스크립트 코드 삽입 방지
-                .secure(false) // https 에서 암호화된 요청
+                .secure(true) // https 에서 암호화된 요청
                 .sameSite("None") // 서로 다른 도메인 간의 쿠키 전송에 대한 보안
-//                .path() // 요청 URL에 반드시 포함되어야 하는 경로, 해당 URL이 없으면 쿠기를 보낼 수 없음
+//                .path() // 요청 URL에 반드시 포함되어야 하는 경로, 해당 URL이 없으면 쿠키를 보낼 수 없음
                 .build();
     }
 
