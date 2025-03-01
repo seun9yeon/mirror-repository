3e34d7914649d9867f99c6345766c6251903cf4c
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2025-03-02 01:33:00 +0900

    [BK-84] [fix] 쿠키 임시로 수정 (#73)

--- backend/src/main/java/org/example/book_report/dto/response/TokenResponseDto.java
+++ backend/src/main/java/org/example/book_report/dto/response/TokenResponseDto.java
@@ -26,7 +26,8 @@ public class TokenResponseDto {
                 .from(ACCESS_TOKEN, this.accessToken)
                 .httpOnly(true) // XSS(Cross site scripting) attack 방지, 스크립트 코드 삽입 방지
                 .secure(secure) // https 에서 암호화된 요청
-                .sameSite("None") // 서로 다른 도메인 간의 쿠키 전송에 대한 보안
+//                .sameSite("None") // 서로 다른 도메인 간의 쿠키 전송에 대한 보안
+                .sameSite("Lax") // 서로 다른 도메인 간의 쿠키 전송에 대한 보안
                 .path("/") // 요청 URL에 반드시 포함되어야 하는 경로, 해당 URL이 없으면 쿠키를 보낼 수 없음
                 .maxAge(tokenValidityInSeconds)
                 .build();
