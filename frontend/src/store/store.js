94887a4c6b13d796e6e70f3965690226507a4450
apricity2u <gkdms9849@gmail.com>
2025-03-02 00:38:50 +0900

    [BK-82] [fix] slice import 오류 해결 (#72)

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './slices/authSlice';
-import selectedCard from './slices/selectedCardSlice';
+import selectedCardSlice from './slices/selectedCardSlice';
 
 const store = configureStore({
   reducer: {
