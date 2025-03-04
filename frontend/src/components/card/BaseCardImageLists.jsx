3aac49de096f0dda236ae66c223fda04e01d878c
apricity2u <gkdms9849@gmail.com>
2025-03-04 14:33:08 +0900

    [BK-101] [refactor] 기본 이미지 오류 해결 (#95)

--- frontend/src/components/card/BaseCardImageLists.jsx
+++ frontend/src/components/card/BaseCardImageLists.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from '../../styles/Card.module.css';
 import { useDispatch } from 'react-redux';
 import { selectCard } from '../../store/slices/selectedCardSlice';
 
 export default function BaseCardImageLists() {
-  const imageFiles = Array.from({ length: 9 }, (_, i) => `base${i + 1}.png`);
+  const imageFiles = Array.from({ length: 9 }, (_, i) => ({
+    fileName: `base${i + 1}.png`,
+    imageId: i + 93781,
+  }));
   const getImageUrl = (fileName) => new URL(`../../assets/${fileName}`, import.meta.url).href;
 
   const dispatch = useDispatch();
 
-  function handleClickImage(e) {
-    dispatch(selectCard({ imageId : -1, imageUrl: e.target.src }));
+  function handleClickImage(imageId, imageUrl) {
+    dispatch(selectCard({ imageId, imageUrl }));
   }
 
   return (
     <ul className={styles.selectCardImageSection}>
-      {imageFiles.map((image) => (
-        <li key={image} className={styles.cardImageStyle}>
-          <img src={getImageUrl(image)} alt="" onClick={handleClickImage} />
+      {imageFiles.map(({ fileName, imageId }) => (
+        <li key={fileName} className={styles.cardImageStyle}>
+          <img
+            src={getImageUrl(fileName)}
+            alt=""
+            onClick={() => handleClickImage(imageId, getImageUrl(fileName))}
+          />
         </li>
       ))}
     </ul>
