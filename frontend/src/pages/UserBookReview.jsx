ba5da8282015470684bc71dda1db860ce69270ab
ysc <ysc13245@gmail.com>
2025-03-05 01:40:33 +0900

    [BK-94] [feat] WIP 유저페이지

--- frontend/src/pages/UserBookReview.jsx
+++ frontend/src/pages/UserBookReview.jsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import authApi from '../api/authApi';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-export default function MyBookReview() {
-  return <div>MyBookReview</div>;
+export default function UserBookReview() {
+  const { username } = useParams();
+  const navigate = useNavigate();
+  const loginUsername = useSelector((state) => state.auth.username);
+  const [userReviews, setUserReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchUserReviews = async () => {
+      try {
+        const reviews = await authApi.getUserReviews("opopop");
+        setUserReviews(reviews.data.userBookReviews);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    
+    fetchUserReviews();
+  }, []);
+
+  return (
+    <>
+      {userReviews.length > 0 ? (
+        userReviews.map((review) => (
+          <h1 key={review.id}>{review.title}</h1>
+        ))
+      ) : (
+        <p>리뷰가 없습니다.</p>
+      )}
+    </>
+  );
 }
